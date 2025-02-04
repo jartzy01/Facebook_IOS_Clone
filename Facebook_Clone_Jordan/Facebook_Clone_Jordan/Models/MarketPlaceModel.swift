c440c9f0ec559022c389a80a8200597a19ceb973
jordan artzy-mccendie <127543411+jartzy01@users.noreply.github.com>
2025-02-04 18:04:08 -0500

    Add files via upload

---
+++ Facebook_Clone_Jordan/Facebook_Clone_Jordan/Models/MarketPlaceModel.swift
@@ -0,0 +1,15 @@
+//
+//  MarketPlaceModel.swift
+//  Facebook_Clone_Jordan
+//
+//  Created by english on 2025-02-04.
+//
+
+import Foundation
+
+struct MarketData: Identifiable{
+    var id = UUID()
+    var imageName: String
+    var title: String
+    var price: String
+}
---
+++ Facebook_Clone_Jordan/Facebook_Clone_Jordan/Models/NotificationsModel.swift
@@ -0,0 +1,17 @@
+//
+//  NotificationsModel.swift
+//  Facebook_Clone_Jordan
+//
+//  Created by english on 2025-02-04.
+//
+
+import Foundation
+
+struct NotificationData {
+    var id: Int
+    var profileImage: String
+    var username: String
+    var timestamp: String
+    var message: String
+    var iconImage: String
+}
