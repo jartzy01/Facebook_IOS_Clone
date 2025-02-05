4b2a02a0a107bd20f1ddfe819e0ef9f96eadbd2a
jordan artzy-mccendie <127543411+jartzy01@users.noreply.github.com>
2025-02-05 17:58:23 -0500

    adding & updating models

---
+++ Facebook_Clone_Jordan/Facebook_Clone_Jordan/Models/VideosModel.swift
@@ -0,0 +1,19 @@
+//
+//  VideosModel.swift
+//  Facebook_Clone_Jordan
+//
+//  Created by english on 2025-02-05.
+//
+
+import Foundation
+
+struct VideoData: Identifiable {
+    var id: Int
+    var VideoImage: String
+    var VideoDesc: String
+    var Likes: String
+    var Comments: String
+    var Shares: String
+    var ProfileImage: String
+    var ProfileName: String
+}
