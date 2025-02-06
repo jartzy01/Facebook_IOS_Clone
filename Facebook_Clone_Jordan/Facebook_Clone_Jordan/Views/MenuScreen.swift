b2436593a595a4f81dc26655fe0769b851fe4e53
jordan artzy-mccendie <127543411+jartzy01@users.noreply.github.com>
2025-02-05 19:02:51 -0500

    update

--- Facebook_Clone_Jordan/Facebook_Clone_Jordan/Views/MenuScreen.swift
+++ Facebook_Clone_Jordan/Facebook_Clone_Jordan/Views/MenuScreen.swift
@@ -8,6 +8,21 @@
 import SwiftUI
 
 struct MenuScreen: View {
+    let items = [
+        (image: "suit.heart.fill", label: "Dating"),
+        (image: "clock.arrow.circlepath", label: "Memories"),
+        (image: "bookmark.fill", label: "Saved"),
+        (image: "person.3.fill", label: "Groups"),
+        (image: "play.tv.fill", label: "Video"),
+        (image: "storefront.fill", label: "Marketplace"),
+        (image: "magnifyingglass", label: "Find friends"),
+        (image: "newspaper", label: "Feeds")
+    ]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
     var body: some View {
         NavigationStack{
             ScrollView{
@@ -28,14 +43,76 @@ struct MenuScreen: View {
                             .foregroundColor(.black)
                             .padding(.trailing)
                     }
-                    HStack{
-                        
+                    VStack {
+                        HStack{
+                            Image("demo")
+                                .resizable()
+                                .clipShape(Circle())
+                                .frame(width: 50, height: 50)
+                            Text("Jordan Artzy-Mccendie")
+                                .font(.system(size: 18, weight: .medium))
+                                .foregroundStyle(.black)
+                            Spacer()
+                            Image(systemName: "chevron.down")
+                                .resizable()
+                                .foregroundStyle(.black)
+                                .frame(width: 12, height: 10)
+                                .background(
+                                    Circle()
+                                        .fill(Color.gray.opacity(0.4))
+                                        .frame(width: 40, height: 40)
+                                )
+                                .padding()
+                        }
+                        Divider()
+                            .padding(7)
+                        HStack{
+                            Image(systemName: "plus.circle.fill")
+                                .resizable()
+                                .foregroundStyle(.black.opacity(0.6))
+                                .frame(width: 40, height: 40)
+                            VStack{
+                                Text("Create new profile or Page")
+                                    .foregroundStyle(.black.opacity(0.6))
+                                    .font(.system(size: 18.2, weight: .medium))
+                                Text("Switch between profiles with one login.")
+                                    .foregroundStyle(.black.opacity(0.6))
+                                    .font(.system(size: 12, weight: .light))
+                            }
+                            Spacer()
+                        }
                     }
+                    .padding()
+                    .background(RoundedRectangle(cornerRadius: 15).fill(Color.white).frame(width: 380))
                     
+                    LazyVGrid(columns: columns, spacing: 10) {
+                         ForEach(items, id: \.label) { item in
+                             HStack{
+                                 VStack {
+                                     Image(systemName: item.image)
+                                         .resizable()
+                                         .scaledToFit()
+                                         .frame(width: 150, height: 20)
+                                         .foregroundColor(.blue)
+                                         .padding(.leading)
+                                         .padding(.trailing)
+                        
+                                     Text(item.label)
+                                         .font(.headline)
+                                         .foregroundColor(.black)
+                                 }
+                                 .frame(height: 80)  // Set the height for each item in the grid
+                                 .background(Color.gray.opacity(0.3))
+                                 .cornerRadius(15)
+                                 .shadow(radius: 5)
+                              
+                             }
+                         }
+                     }
                 }
 
             }
-            .background(Color.gray.ignoresSafeArea())
+            .background(Color.gray.opacity(0.4).ignoresSafeArea())
         }
     }
 }
